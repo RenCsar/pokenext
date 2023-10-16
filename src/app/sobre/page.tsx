9aaeed41e261df95ebb419ca876d54b5bc28b3f1
Renan Cesar <cesar.renan@hotmail.com>
2023-10-16 14:44:45 -0300

    feat: finalizando a págia about

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
+import Family from './../../../public/images/fam.webp'
 
 const Sobre = () => {
   return (
     <main className={"flex min-h-[550px] flex-col items-center justify-center"}>
-      <h1 className="text-[28px] ">Sobre</h1>
+      <h1 className="text-[28px] font-bold">Sobre o Projeto</h1>
+      <p className='mb-8 text-[18px]'>PokeNext é um App construído em Next.js para consultar Pokémons.</p>
+      <Image
+        src={Family}
+        alt="Charizard"
+        width="800"
+        height="800"
+      />
     </main>
   )
 }
 
-export default Sobre
+export default Sobre;
